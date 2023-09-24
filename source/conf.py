516da6df3b0b661677f632071ac478f23ce09864
nanli <linanenv@gmail.com>
2023-09-24 16:24:34 +0000

    change theme

--- source/conf.py
+++ source/conf.py
@@ -6,22 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'TianGong-AI-Wiki'
-copyright = '2023, Nan LI'
-author = 'Nan LI'
+project = "TianGong-AI-Wiki"
+copyright = "2023, Nan LI"
+author = "Nan LI"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
